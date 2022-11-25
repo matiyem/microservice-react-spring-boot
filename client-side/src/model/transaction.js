ffcad85ecbd607041da2237329883e683621a18c
matiyem <39719384+matiyem@users.noreply.github.com>
2022-11-25 21:43:24 +0330

    Add files via upload

---
+++ client-side/src/model/transaction.js
@@ -0,0 +1,8 @@
+export class Transaction{
+    constructor(userId,course,dateOfIssue,id) {
+        this.userId=userId;
+        this.course=course;
+        this.dateOfIsuue=dateOfIssue;
+        this.id=id;
+    }
+}
---
+++ client-side/src/model/user.js
@@ -0,0 +1,9 @@
+export class User{
+    constructor(username,password,name,id) {
+        this.username=username;
+        this.password=password;
+        this.name=name;
+        this.id=id;
+
+    }
+}
